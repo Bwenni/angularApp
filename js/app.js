be0ac8032ab7d959fd62aa4379c58e2362af76d8
Menno Witsen <menno_witsen@hotmail.com>
2013-11-24 18:17:10 +0100

    angular score app

--- js/app.js
+++ js/app.js
@@ -1,20 +1,29 @@
+//Mandates you to write better code that does not throw exceptions.
 'use strict';
 
-var scoreApp = angular.module('scoreApp', ['angular-gestures']);
+//Decleration of the application. This is the namespace function. Also includes external libraries in your applications 
+var scoreApp = angular.module('scoreApp', ['hmTouchEvents','ng-iscroll','ngProgress']);
 
-(function(){
+//Router, gets the url parameters and directs to the right template and controler.
+scoreApp.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+   delete $httpProvider.defaults.headers.common['X-Requested-With'];
+   $routeProvider.
+      when('/pools', {templateUrl: 'partials/pools.html', controller: 'PoolCtrl'}).
+      when('/schedule', {templateUrl: 'partials/schedule.html', controller: 'ScheduleCtrl'}).
+      when('/team/:teamID', {templateUrl: 'partials/team.html',   controller: 'TeamCtrl'}).
+      when('/scores/:matchID', {templateUrl: 'partials/scores.html',   controller: 'ScoreCtrl'}).
+      when('/addscore/:matchID', {templateUrl: 'partials/addscore.html',   controller: 'ScoreCtrl'}).
+      otherwise({redirectTo: '/pools'});
+   }]);
 
-	scoreApp.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
-	  delete $httpProvider.defaults.headers.common['X-Requested-With'];
-	  $routeProvider.
-	      when('/home', {templateUrl: 'partials/home.html',   controller: 'HomeCtrl'}).
-	      when('/pools', {templateUrl: 'partials/pools.html',   controller: 'PoolCtrl'}).
-	      when('/schedule', {templateUrl: 'partials/schedule.html',   controller: 'ScheduleCtrl'}).
-	      when('/team/:teamID', {templateUrl: 'partials/team.html',   controller: 'TeamCtrl'}).
-	      when('/match/:matchID', {templateUrl: 'partials/match.html',   controller: 'MatchCtrl'}).
-	      when('/scores/:matchID', {templateUrl: 'partials/scores.html',   controller: 'ScoreCtrl'}).
-	      when('/addscore/:matchID', {templateUrl: 'partials/addscore.html',   controller: 'ScoreCtrl'}).
-	      otherwise({redirectTo: '/pools'});
-	}]);
+//Global variables used in the application
+var config = {
+    api_url : 'https://api.leaguevine.com/v1/',
+    acces_token : 'access_token=0820ecf70',
+    tournament_id : '19389',
+    tournament_name : 'amsterdam ultimate',
+    loader_height : '10px',
+    loader_color : 'Navy'
+}
 
-})();
+
--- js/controllers.js
+++ js/controllers.js
@@ -1,205 +1,252 @@
-'use strict'; 
+//Master application controller. 
+scoreApp.controller('MainController', function ($scope, $location){
+   //Binds active classes to the menu.
+   $scope.isActive = function(route) {
+      return route === $location.path();
+   }
+});
 
-/* Controllers */
+//Pool controller. Includes all 
+scoreApp.controller('PoolCtrl', function ($scope, $http, $location, ngProgress, updateIscroll, ajaxCall) {
+   //Gets the data from the localstorage
+    var poolData = JSON.parse(localStorage.getItem('poolData'));
 
-(function(){ //Begin self invoking anonymous function
+    //Checks if there is any data in the localstorage
+    if(poolData){
+         //Bind the data to the page
+         $scope.pools = poolData.objects;
+         updateIscroll($scope, 'pools');
+    }else{
+        getData();
+    }
+    //Get new data from the api.
+    function getData(){
+        //Starts the progressbar.
+        progressBarInit(ngProgress);
+        //Set the parameters
+        var pools_call = 'pools/?tournament_id='+config.tournament_id+'&limit=30&';
+       
+        var myDataPromise = ajaxCall.getAllData(pools_call, 'poolData');
 
-	var config = {
-		api_url : 'https://api.leaguevine.com/v1/',
-		acces_token : 'access_token=0820ecf70',
-	    tournament_id : '19389',
-	    tournament_name : 'Amsterdam Ultimate'
-	}
+        myDataPromise.then(function(result) {
+            $scope.pools = result.objects;
+            ngProgress.complete();
+        });
 
-	var keep = {
-		upcoming : false,
-		latest : false
-	}
+        //Update the iscroll
+        updateIscroll($scope, 'pools');
+    }
+    
+    //Refresh button listener.
+    $scope.refreshData = function(){
+        getData();
+    }
+    
+    //Button listener and directs to another page.
+    $scope.showTeam = function(id) {
+        $location.path('/team/'+id);
+    }
 
+    //Button listener and directs to another page.
+    $scope.showSchedule = function(){
+        $location.path('/schedule/');
+    }
 
-	scoreApp.controller('Main', ['$scope', '$location', function ($scope, $location, $routeChangeStart) {
-		$scope.menu = 'hide';
+    //When the orientation of the device changes the content changes.
+    orientationSwitch();
+    function orientationSwitch(){
+        switch(window.orientation){  
+          case -90:
+            $scope.portraitVar = '';
+            $scope.$apply()
+            break;
+          case 90:
+              $scope.portraitVar = '';
+              $scope.$apply()
+              break; 
+          case 0:
+            $scope.portraitVar = 'portrait';
+            $scope.$apply()
+            break; 
+        }
+    }
+    window.addEventListener("orientationchange", function() {
+        orientationSwitch();
+    }, false);
 
-		$scope.isActive = function(route) {
-			return route === $location.path();
-		};
+});
 
-		$scope.header = config.tournament_name;
+//Schedule controller.
+scoreApp.controller('ScheduleCtrl', function ($scope, $http, $location, ngProgress, updateIscroll, ajaxCall) {
+    var scheduleData = JSON.parse(localStorage.getItem('scheduleData'));
+    if(scheduleData){
+        $scope.schedule = scheduleData.objects;
+        updateIscroll($scope, 'schedule');
+    }else{
+        getData();
+    }
+    
+    function getData(){
+        progressBarInit(ngProgress);
 
-		$scope.showMenu =  function() {
-			if ($scope.menu == 'hide') {
-				$scope.menu = 'show';
-			} else {
-				$scope.menu = 'hide';
-			}
-		}
-	}]);
+        var schedule_call = 'games/?tournament_id='+config.tournament_id+'&order_by=%5Bstart_time%5D&';
+       
+        var myDataPromise = ajaxCall.getAllData(schedule_call, 'scheduleData');
 
-	scoreApp.controller('HomeCtrl', function ($scope, $http) {
-		$scope.filter = 'latest';
+        myDataPromise.then(function(result) {
+            $scope.schedule = result.objects;
+            ngProgress.complete();
+        });
+        updateIscroll($scope, 'schedule');
+    }
+    
+    $scope.refreshData = function(){
+        getData();
+    }
 
-		if (keep.latest == false) {
-			$scope.displayLoadingIndicator = true;
+    $scope.showPool = function(){
+        $location.path('/pools/');
+    }
 
-			$http.get('https://api.leaguevine.com/v1/game_scores/?tournament_id='+config.tournament_id+'&limit=20&'+config.acces_token).success(function(data) {		
-				$scope.latest = data.objects;
-				console.log($scope.latest);
-				$scope.displayLoadingIndicator = false;
-				keep.latest = $scope.latest;
-			});
-		} else {
-			$scope.latest = keep.latest;
-		}
-		if (keep.upcoming == false) {
-			$http.get('https://api.leaguevine.com/v1/games/?tournament_id='+config.tournament_id+'&order_by=%5Bstart_time%5D&limit=20&'+config.acces_token).success(function(data) {		
-				$scope.upcoming = data.objects;
-				console.log($scope.upcoming);
-				keep.upcoming = $scope.upcoming;
-			});
-		} else {
-			$scope.upcoming = keep.upcoming;
-		}
-		$scope.viewOptions = function(ev){
-			angular.element(ev.currentTarget).parent().toggleClass('active');
-		}	
-		$scope.toDate = function(date) {
-			var time = new Date(date);
-			var day = time.getDay();
-			var month = time.getDate();
-			var hours = time.getHours();
-			var minutes = (time.getMinutes()<10?'0':'') + time.getMinutes();
-			return day + '/' + month + ' om ' + hours + ':' + minutes;
-		}
-	});
+    $scope.showScore = function(id) {
+        $location.path('/scores/'+id);
+    }
 
-	scoreApp.controller('PoolCtrl', function ($scope, $http, $location) {
-		$http.get(config.api_url+'pools/?tournament_id='+config.tournament_id+'&limit=30&'+config.acces_token).success(function(data) {
-			$scope.pools = data.objects;
-		});
-		
-		$scope.showTeam = function(id) {
-			$location.path('/team/'+id);
-		}
-	});
+   //Changes the datetime from the api to a readable date
+    $scope.toDate = function(date) {
+        var time = new Date(date);
+        var day = time.getDate();
+        var month = time.getMonth()+1;
+        return day + '-' + month;
+    }
 
-	scoreApp.controller('ScheduleCtrl', function ($scope, $http, $location) {
-		$http.get(config.api_url+'game_scores/?tournament_id='+config.tournament_id+'&order_by=%5Btime%5D&access_token'+config.acces_token).success(function(data) {
-			$scope.schedule = data.objects;
-			console.log(data.objects);
-		});
+    //Changes the datetime from the api to a readable time
+    $scope.toTime = function(date) {
+        var time = new Date(date);
+        var hours = time.getHours();
+        var minutes = (time.getMinutes()<10?'0':'') + time.getMinutes();
+        return hours + ':' + minutes;
+    }
+    
+});
 
-		$scope.showScore = function(id) {
-			$location.path('/scores/'+id);
-		}
+//Score controller
+scoreApp.controller('ScoreCtrl', function ($scope, $http, $location, $routeParams, ngProgress, updateIscroll, ajaxCall) {
+    //Retrieves the id from the url
+    $scope.matchID = $routeParams.matchID;
 
-		$scope.toDate = function(date) {
-			var time = new Date(date);
-			var day = time.getDate();
-			var month = time.getMonth()+1;
-			return day + '-' + month;
-		}
+    var scoreData = JSON.parse(localStorage.getItem('scoreData'));
+    if(scoreData){
+        if(scoreData.game_id === $scope.matchID){
+            $scope.scores = scoreData.objects;
+            updateIscroll($scope, 'scores');    
+        }else{
+             getData();
+        }
+    }else{
+        getData();
+    }
+    
 
-		$scope.toTime = function(date) {
-			var time = new Date(date);
-			var hours = time.getHours();
-			var minutes = (time.getMinutes()<10?'0':'') + time.getMinutes();
-			return hours + ':' + minutes;
-		}
-	});
+    function getData(){
+        progressBarInit(ngProgress);
 
-	//https://api.leaguevine.com/v1/games/?tournament_id=19389&team_ids=%5B21204%5D&fields=%5Bstart_time%2Cteam_1%2Cteam_2%2Cteam_1_score%2Cteam_2_score%5D&access_token=e12d7b28f4
-scoreApp.controller('ScoreCtrl', function ($scope, $http, $location, $routeParams) {
-	
-	$scope.matchID = $routeParams.matchID;
+        var score_call = 'game_scores/?game_id='+$scope.matchID+'&fields=%5Bis_final%2Cteam_1%2Cteam_1_score%2Cteam_2%2Cteam_2_score%5D&limit=200&';
 
-	$http.get(config.api_url+'game_scores/?game_id='+$scope.matchID+'&fields=%5Bis_final%2Cteam_1%2Cteam_1_score%2Cteam_2%2Cteam_2_score%5D&limit=200&'+config.acces_token).success(function(data) {
-		$scope.scores = data.objects;
-		console.log($scope.scores);
-		
-	});
+        var myDataPromise = ajaxCall.getAllData(score_call, 'scoreData',$scope.matchID);
 
-	$scope.submitScore = function() {
+        myDataPromise.then(function(result) {
+            $scope.scores = result.objects;
+            ngProgress.complete();
+        });
+        updateIscroll($scope, 'scores');
+    }
+    
+    $scope.refreshData = function(){
+        getData();
+    }
 
-		if ($scope.team1score == '' || $scope.team2score == '') {
-			alert('Vul alle velden in');
-			return false;
-		}
+    $scope.showscores = function() {
+        $scope.show = 'true';
+    };
 
-		$scope.displayWrapper = false;
-		$scope.displayLoadingIndicator = true;
+    $scope.showSchedule = function(){
+        $location.path('/schedule/');
+    }
 
-		var postData = {
-	      "game_id": $scope.matchID,
-		  "team_1_score": $scope.team1score,
+    //Toggle function for addscore 
+    $scope.custom = true;
+    $scope.toggleCustom = function() {
+        $scope.custom = $scope.custom === false ? true: false;
+         updateIscroll($scope, 'scores');
+    };
+
+    //Submit score function
+    $scope.submitScore = function() {
+
+        var postData = {
+          "game_id": $scope.matchID,
+          "team_1_score": $scope.team1score,
           "team_2_score": $scope.team2score,
           "is_final": $scope.isfinal
-	    };
-	    $http({
-	      method: 'POST',
-	      url: 'https://api.leaguevine.com/v1/game_scores/',
-	      data: postData,
-	      headers: {
-	      	'Content-Type': 'application/json',
-			'Accept': 'application/json',
-			'Authorization': 'bearer 82996312dc'
-	      },
-	    }).
-	    success(function(response) {
-	    	console.log('succes');	
-	    	window.location.hash = "/scores/"+$scope.matchID;
-	    }).
-	    error(function(response) {
-	        console.log('failed');	
-	    	console.log(response);
-	    });
-	};
+        };
+        //Sets the ajax post call to the api
+        $http({
+          method: 'POST',
+          url: config.api_url+'game_scores/',
+          data: postData,
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'bearer 82996312dc'
+          },
+        }).
+        success(function(response) {
+            console.log('succes');  
+            getData();
+            updateIscroll($scope, 'scores');
+        }).
+        error(function(response) {
+            console.log('failed');  
+        });
+    };
 });
-//https://api.leaguevine.com/v1/game_scores/?game_id=127153&tournament_id=19389&fields=%5Bis_final%2Cteam_1%2Cteam_1_score%2Cteam_2%2Cteam_2_score%5D&access_token=aa938b6531
-	scoreApp.controller('MatchCtrl', function ($scope, $http, $location, $routeParams) {
-		$scope.displayWrapper = false;
-		$scope.displayLoadingIndicator = true;
 
-		$scope.matchID = $routeParams.matchID;
+//Team Controller
+scoreApp.controller('TeamCtrl', function ($scope, $http, $routeParams, $location, ngProgress, updateIscroll, ajaxCall) {
+    $scope.teamID = $routeParams.teamID;
 
-		$http.get(config.api_url+'games/'+$scope.matchID+'/?'+config.acces_token).success(function(data) {
-			$scope.match = data;
-			console.log($scope.match);
-			$scope.displayLoadingIndicator = false;
-			$scope.displayWrapper = true;
-		});
-	});
+    getData();
 
-	// scoreApp.controller('ScoreCtrl', function ($scope, $http, $location, $routeParams) {
-	// 	$scope.displayWrapper = false;
-	// 	$scope.displayLoadingIndicator = true;
+    function getData(){
+        progressBarInit(ngProgress);
+       
+        var teams_call = 'games/?tournament_id='+config.tournament_id+'&team_ids=['+$scope.teamID+']&';
+       
+        var myDataPromise = ajaxCall.getAllData(teams_call, 'teamData');
 
-	// 	$scope.matchID = $routeParams.matchID;
+        myDataPromise.then(function(result) {
+            if(result.objects[0].team_1_id == $scope.teamID){
+                $scope.team_name = result.objects[0].team_1.name;
+            }else{
+                $scope.team_name = result.objects[0].team_2.name;
+            }
+            $scope.teams = result.objects;
+            ngProgress.complete();
+            updateIscroll($scope, 'teams');
+        });
+        
+    }
+    
+    $scope.refreshData = function(){
+        getData();
+    }
 
-	// 	$http.get(config.api_url+'games/'+$scope.matchID+'/?'+config.acces_token).success(function(data) {
-	// 		$scope.match = data;
-	// 		console.log($scope.match);
-	// 		$scope.displayLoadingIndicator = false;
-	// 		$scope.displayWrapper = true;
-	// 	});
+    $scope.showPool = function(){
+        $location.path('/pools/');
+    }
 
-		
-	// });
-
-	scoreApp.controller('TeamCtrl', function ($scope, $http, $routeParams, $location) {
-		$scope.teamID = $routeParams.teamID;
-		$http.get(config.api_url+'teams/'+$scope.teamID+'/?'+config.acces_token).success(function(data) {
-			$scope.team = data;
-		});
-
-		$http.get('https://api.leaguevine.com/v1/games/?tournament_id='+config.tournament_id+'&team_ids=['+$scope.teamID+']&'+config.acces_token).success(function(data) {
-			$scope.scores = data.objects;
-		});
-
-		$scope.showTeam = function(id) {
-			$location.path('/team/'+id);
-		}
-
-	});
-
-
-})(); //Eind self invoking anonymous function
+    $scope.showTeam = function(id) {
+        $location.path('/team/'+id);
+    }
+});
